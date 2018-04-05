ad85c0b026025d3ac18d15d93b5d12f34b375538
sevenflash <sevenflash@yandex.ru>
2018-04-06 00:53:53 +0300

    init

---
+++ conf.js
@@ -0,0 +1,4 @@
+exports.deviceName = 'Boilerplate bot';
+exports.permanent_pairing_secret = '0000';
+exports.storage = 'sqlite';
+exports.hub = 'byteball.org/bb';
---
+++ index.js
@@ -0,0 +1,23 @@
+const headlessWallet = require("headless-byteball");
+
+const device = require("byteballcore/device");
+const eventBus = require("byteballcore/event_bus");
+const db = require("byteballcore/db");
+
+const messages = require("./messages.json");
+
+eventBus.once("headless_wallet_ready", () => {
+	console.log('READY');
+});
+
+eventBus.on("paired", (from) => {
+	reply(from, messages.welcome, "Welcome");
+});
+
+eventBus.on("text", (from, text) => {
+	reply(from, text);
+});
+
+function reply(recipient, message, subject = "") {
+	device.sendMessageToDevice(recipient, subject, message);
+}
